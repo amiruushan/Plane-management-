814719831eab91adb5065980c60867d1f03dfcb1
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2024-08-03 20:15:41 +0530

    create person class

---
+++ Person.java
@@ -0,0 +1,44 @@
+public class Person {
+
+    // declaring variables for attributes of a person
+    private String name;
+    private String surname;
+    private String email;
+
+    Person(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    // creating method for getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void PersonInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Email: " + email);
+    }
+
+}
